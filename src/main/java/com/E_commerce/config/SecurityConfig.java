1a0285b85e80f76e5e7c7d6725ab91eefbcbf9a1
Kiruthika-S-dev <kiruthikasaravanan2005@gmail.com>
2026-04-21 23:09:33 +0530

    Fix COrs with Customizer

--- src/main/java/com/E_commerce/config/SecurityConfig.java
+++ src/main/java/com/E_commerce/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -29,7 +30,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http)
             throws Exception {
         http
-        .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+        .cors(Customizer.withDefaults())
         .csrf(csrf -> csrf.disable())
             .sessionManagement(session -> session
                 .sessionCreationPolicy(
