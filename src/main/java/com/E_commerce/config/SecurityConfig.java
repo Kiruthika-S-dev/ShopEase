2026b0679fd38e3b0385f09170af9f720d79ce3e
Kiruthika-S-dev <kiruthikasaravanan2005@gmail.com>
2026-04-21 22:26:27 +0530

    Fix CORS issue

--- src/main/java/com/E_commerce/config/SecurityConfig.java
+++ src/main/java/com/E_commerce/config/SecurityConfig.java
@@ -63,6 +63,7 @@ public class SecurityConfig {
             "http://localhost:5500",
             "http://127.0.0.1:5500",
             "https://dynamic-capybara-3ea090.netlify.app",
+            
             "https://remarkable-hotteok-a5a918.netlify.app"
         ));
 
